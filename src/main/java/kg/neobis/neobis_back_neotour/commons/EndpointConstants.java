45de13ef288fd25614e4c645ba19c6b7d40304e5
imashevdt11 <imashevdt@gmail.com>
2024-04-23 18:07:53 +0600

    little changes

--- src/main/java/kg/neobis/neobis_back_neotour/commons/EndpointConstants.java
+++ src/main/java/kg/neobis/neobis_back_neotour/commons/EndpointConstants.java
@@ -1,8 +1,8 @@
-package com.project.neobis_back_neotour.commons;
+package kg.neobis.neobis_back_neotour.commons;
 
 public final class EndpointConstants {
 
-    public static final String API_PREFIX = "/api";
+    private static final String API_PREFIX = "/api";
 
     public static final String USER_ENDPOINT = API_PREFIX + "/users";
 
